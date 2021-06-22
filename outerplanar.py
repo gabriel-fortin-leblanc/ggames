15eaf8abdbb87f5a6641acf125fa54ff8f72da05
gf-math-info <gabriel.fortin-leblanc@umontreal.ca>
2021-06-22 15:40:33 -0400

    get_game_graph function implemented

--- outerplanar.py
+++ outerplanar.py
@@ -2,11 +2,12 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 
+import math
 import time
 import itertools
 
 
-def get_attractor(V, E, V0, F0):
+def get_attractor(V, A, V0, F0):
     """
     Compute the attractor set.
     Credit : Dietmar Berwanger in "Graph games with perfect information"
@@ -35,18 +36,79 @@ def get_attractor(V, E, V0, F0):
         p[v] = set()
         n[v] = 0
 
-    for u, v in E:
+    for u, v in A:
         p[v].add(u)
         n[u] += 1
 
     for v in F0:
         propagate(v, w, p, n)
-    return w
+
+    return [node for node, in_attractor in w.items() if in_attractor]
 
 
-def get_game_graph(G, tau):
-    pass
+def get_game_graph(V, E, tau, k, T = None):
+    """
+    Compute the game graph where the k-cops and robber game takes place on the
+    edge periodic graph (V, E, tau) with a time horizon T.
+    :param V: The list of vertices
+    :param E: The list of edges
+    :param tau: The presence function of the edges in E in dict
+    :param k: The number of cops in the game
+    :param T: The time horizon which is the least common multiple of the
+              values of tau.
+    """
+    if T is None:
+        # Compute the least common multiple.
+        pattern_length = map(len, tau.values())
+        T = math.prod(pattern_length) // math.gcd(pattern_length)
 
+    V_gg = [], A_gg = []
+    V0 = set(), F0 = set()
+    capture_flag = False
+
+    for *c, r in itertools.product(V, repeat=k+1):
+        V0.add((*c, r, False, 0))
+
+        # All the following nodes are initial ones.
+        capture_flag = r in c
+
+        for s in [False, True]:
+            for t in range(T):
+                V_gg.append((*c, r, s, t))
+                if capture_flag:
+                    F0.add((*c, r, s, t))
+
+    for u, v in itertools.product(V_gg, repeat=2):
+        *c0, r0, s0, t0 = u
+        *c1, r1, s1, t1 = v
+        if t0 == t1 and not s0 and s1 and r0 == r1:
+            # Cop's move
+            valid_flag = True
+            for i in range(len(c0)):
+                if c0[i] != c1[i] and \
+                        (c0[i], c1[i]) not in E and \
+                        (c1[i], c0[i]) not in E:
+                    # It is impossible for a cops to move in
+                    # one rounds to a non adjacent vertex.
+                    valid_flag = False
+                    break
+            if valid_flag:
+                A_gg.append((u, v))
+        
+        elif t0 + 1 == t1 and s0 and not s1 and c0 == c1:
+            # Robber's move
+            if r0 == r1 or (r0 != r1 and
+                    (((r0, r1) in E and tau[(r0, r1)][t0] == '1') or
+                    ((r1, r0) in E and tau[(r1, r0)][t0] == '1'))):
+                A_gg.append((u, v))
+    return V_gg, A_gg, V0, F0
+
+
+def get_winning_strategy(attractor):
+    strategies = []
+    for init_state in [(*crs, t) for *crs, t in attractor if t == 0]:
+        pass
+    
 
 T = 1
 # Generate vertices
@@ -72,50 +134,3 @@ for u, v in G.edges:
     b = np.random.randint(2, size=T)
     tau[(u, v)] = b
     tau[(v, u)] = b
-
-# Compute the game graph (gg)
-vertices_gg = []
-edges_gg = []
-F0 = set()
-V0 = set()
-for c1 in vertices:
-
-    if c1 != 'C1' and c1 != 'C2':
-        V0.add(('C1', 'C2', c1, False, 0))
-    for c2 in vertices:
-        for r in vertices:
-            for s in [False, True]:
-                for t in range(T):
-                    vertices_gg.append((c1, c2, r, s, t))
-                    if not s:  # Cop's turn
-                        # Both cops don't move
-                        edges_gg.append(((c1, c2, r, False, t), (c1, c2, r, True, t)))
-                        # One of the two cops moves
-                        for n in G.neighbors(c1):
-                            if tau[(c1, n)][t] == '1':
-                                edges_gg.append(((c1, c2, r, False, t), (n, c2, r, True, t)))
-                        for n in G.neighbors(c2):
-                            if tau[(c2, n)][t] == '1':
-                                edges_gg.append(((c1, c2, r, False, t), (c1, n, r, True, t)))
-                        # Both cops move
-                        for n1 in G.neighbors(c1):
-                            if tau[(c1, n1)][t] == '1':
-                                for n2 in G.neighbors(c2):
-                                    if tau[(c2, n2)][t] == '1':
-                                        edges_gg.append(((c1, c2, r, False, t), (n1, n2, r, True, t)))
-                    else:  # Robber's turn
-                        for n in G.neighbors(r):
-                            edges_gg.append(((c1, c2, r, True, t), (c1, c2, n, False, (t + 1) % T)))
-
-                    if c1 == r or c2 == r or (r == 'C1' and c1 != 'C1' and c2 != 'C1') or (
-                            r == 'C2' and c1 != 'C2' and c2 != 'C2'):
-                        F0.add((c1, c2, r, s, t))
-
-GG = nx.DiGraph()
-GG.add_nodes_from(vertices_gg)
-GG.add_edges_from(edges_gg)
-
-Attract_flag = get_attractor(GG.nodes, GG.edges, V0, F0)
-Attrac = {edge for edge, flag in Attract_flag.items()}
-winning = V0.intersection(Attrac)
-print(winning)
