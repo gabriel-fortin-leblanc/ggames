4a873553786bbe1e9ccae0f661c3ff29051c3dd9
gf-math-info <gabriel.fortin-leblanc@umontreal.ca>
2021-07-13 15:31:27 -0400

    main implemented

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from multiprocessing import pool
 import utils
 import cop_robber_game as crg
-from multiprocessing import Lock, Queue
+
+import sys, os, time, re
+import multiprocessing as mp
 
 
 def produce(V, E, sequence_length, k, queue, printing_lock):
@@ -18,7 +21,7 @@ def produce(V, E, sequence_length, k, queue, printing_lock):
     for presence_map in utils.generate_edge_presence_maps(E, sequence_length):
         queue.put((V, E, presence_map, k))
     with printing_lock:
-        print(f'{2**sequence_length**len(E)} problems produced')
+        print(f'{(2**sequence_length-1)**len(E)} problems produced')
 
 def consume(queue, printing_lock, output_file):
     """
@@ -37,10 +40,89 @@ def consume(queue, printing_lock, output_file):
             output_file.write(f'{tau}\n')
 
 
+def str_time_since(start):
+    """
+    Return a string that represents the time passed since "start".
+    :param start: An integer representing the time in nanoseconds
+    """
+    HOURS_TO_NANOSECONDS = 3.6e12
+    MINUTES_TO_NANOSECONDS = 6e10
+    SECONDS_TO_NANOSECONDS = 1e9
+
+    delta_time = time.time_ns() - start_time
+    hours = delta_time // HOURS_TO_NANOSECONDS
+    delta_time -= hours * HOURS_TO_NANOSECONDS
+    minutes = delta_time // MINUTES_TO_NANOSECONDS
+    delta_time -= minutes * MINUTES_TO_NANOSECONDS
+    seconds = delta_time // SECONDS_TO_NANOSECONDS
+    return f'HH:MM:SS = {hours}:{minutes}:{seconds}'
+
+
 if __name__ == '__main__':
-    V = []; E =[]
-    k = 2; sequence_length = 10
-    problem_queue = Queue()
-    print_lock = Lock()
+    ERROR_ARGS_MSG = \
+'''Two file names must specified as argument. The first
+one is a file containing an integer greater than 0 on the first
+line that represents the number of cops, an integer greater than 0
+that represents the length of the edge patterns on the second line
+and a list of adjancy vertices on the third line in the following
+format:\n
+\t(1,2), (2,3), (1,3)\n
+This is a list of couples of vertices.\n
+The second file will contain the output of the program. If the
+file doesn't exist, then it will be created. It contains every
+presence mapping of k-cop-winning graph, one per line.
+*** Don't forget to check the permissions of the files. ***'''
+
+    MAX_SIZE_PROBLEMS_QUEUE = 1000
 
     
+    print('Analyzing the arguments...')
+    start_time = time.time_ns()
+    args = sys.argv
+    if len(args) != 3 or not os.access(args[1], os.R_OK):
+        exit(ERROR_ARGS_MSG)
+    if os.access(args[2], os.F_OK) and not os.access(args[2], os.W_OK):
+        exit(ERROR_ARGS_MSG)
+    
+    V = set(); E = list()
+    k = None; length = None; adjancy = list()
+    file_str = None
+    with open(args[1], 'r') as file:
+        file_str = file.read().split('\n')
+    if len(file_str) != 3:
+        exit(ERROR_ARGS_MSG)
+    try:
+        k = int(re.search(r'\s*(\d+)\s*', file_str[0]).group(0))
+        length = int(re.search(r'\s*(\d+)\s*', file_str[1]).group(0))
+        for edge_str in re.findall(r'\((.+?)\)',
+                file_str[2].replace(' ', '')):
+            edge_str = edge_str.split(',')
+            u = int(edge_str[0]); v = int(edge_str[1])
+            V.add(u); V.add(v)
+            E.append((u, v))
+    except:
+        exit(ERROR_ARGS_MSG)
+    V = list(V)
+    print(f'Analizing completed in {str_time_since(start_time)}')
+    
+    print('Initialization of the producer and the consumers...')
+    start_time = time.time_ns()
+    queue = mp.Queue(MAX_SIZE_PROBLEMS_QUEUE)
+    lock = mp.Lock()
+    file = open(args[2], 'w')
+    producer = mp.Process(target=produce, args=(V, E, length, k, queue, lock))
+    consumers_pool = mp.Pool(initializer=consume,
+            initargs=(queue, lock, file))
+    print(f'Initialization completed in {str_time_since(start_time)}')
+    
+    print('Computing...')
+    start_time = time.time_ns()
+    producer.start()
+    producer.join()
+    while not queue.empty():
+        time.sleep(5)
+    queue.close()
+    queue.join_thread()
+    consumers_pool.close()
+    consumers_pool.join()
+    print(f'Computing completed in {str_time_since(start_time)}')
