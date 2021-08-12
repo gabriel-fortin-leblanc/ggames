6a599b1bb7819617fcabcbcef432ec6661008415
gf-math-info <gabriel.fortin-leblanc@umontreal.ca>
2021-08-12 15:29:45 -0400

    Implement the Vertex and Edge classes

--- src/ggames/graph.py
+++ src/ggames/graph.py
@@ -11,8 +11,17 @@ class Vertex:
 
     __slots__ = ['value']
 
-    def __init__(self, value: Hashable):
-        pass
+    def __init__(self, value: Hashable) -> None:
+        """Build a new vertex."""
+        self = value = value
+
+    def __eq__(self, v: Vertex) -> bool:
+        """Return if v is equal to this vertex."""
+        return self.value == v.value
+
+    def __str__(self) -> str:
+        """Return the string representation of this vertex."""
+        return str(self.value)
 
 
 class Edge:
@@ -24,14 +33,37 @@ class Edge:
 
     def __init__(self, origin: Vertex, destination: Vertex,
                  value: Optional(Hashable)) -> None:
-        pass
+        """
+        Build an edge from the two vertices. It can be considered oriented or
+        not.
+        """
+        self.origin = origin
+        self.destination = destination
+        self.value = value
 
-    def endpoints() -> tuple[Vertex, Vertex]:
-        pass
+    def endpoints(self) -> tuple[Vertex, Vertex]:
+        """Return a tuple containing the vertices origin and destination."""
+        return (self.origin, self.destination)
 
-    def opposite(v: Vertex) -> Vertex:
-        pass
+    def opposite(self, v: Vertex) -> Vertex:
+        """Return the opposite vertex."""
+        if self.origin == v:
+            return self.destination
+        elif self.destination == v:
+            return self.origin
+        else:
+            raise ValueError(f'The vertex {v} is not part of this edge.')
 
+    def __str__(self) -> str:
+        """Return a string representation of this edge."""
+        return f'Edge(origin={self.origin}, destination={self.destination}, '\
+            f'value={self.value})'
+
+    def __eq__(self, e: Edge) -> bool:
+        """Return if the edge e is equal to this one."""
+        return self.origin == e.origin and \
+               self.destination == e.destination and \
+               self.value == e.value
 
 class Graph:
     """
